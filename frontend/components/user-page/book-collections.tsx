1e44c428ed75cf9597564738e7fdd11e56e8c4b7
Gagas Surya <gagassurya19@gmail.com>
2024-11-21 11:35:44 +0700

    out of quota for book

--- frontend/components/user-page/book-collections.tsx
+++ frontend/components/user-page/book-collections.tsx
@@ -13,6 +13,7 @@ export default function BookCollection({ search = "", category = "", years = "" 
                 "Learn how emotions, behaviors, and psychology influence financial decisions and discover timeless lessons for managing money effectively.",
             image:
                 "https://images-na.ssl-images-amazon.com/images/I/81cpDaCJJCL.jpg",
+                quota: 5,
         },
         {
             id: 2,
@@ -24,6 +25,7 @@ export default function BookCollection({ search = "", category = "", years = "" 
                 "A guide to building good habits, breaking bad ones, and achieving remarkable results in everyday life.",
             image:
                 "https://images-na.ssl-images-amazon.com/images/I/91bYsX41DVL.jpg",
+                quota: 5,
         },
         {
             id: 3,
@@ -35,6 +37,7 @@ export default function BookCollection({ search = "", category = "", years = "" 
                 "A brief history of humankind, exploring the journey of humans from the Stone Age to the modern world.",
             image:
                 "https://images-na.ssl-images-amazon.com/images/I/713jIoMO3UL.jpg",
+                quota: 0,
         },
         {
             id: 4,
@@ -46,6 +49,7 @@ export default function BookCollection({ search = "", category = "", years = "" 
                 "A guide for entrepreneurs on how to innovate, test ideas, and build sustainable businesses in a rapidly changing world.",
             image:
                 "https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg",
+                quota: 5,
         },
         {
             id: 5,
@@ -57,6 +61,7 @@ export default function BookCollection({ search = "", category = "", years = "" 
                 "A dystopian novel exploring themes of surveillance, government control, and individual freedom in a totalitarian state.",
             image:
                 "https://images-na.ssl-images-amazon.com/images/I/81t2CVWEsUL.jpg",
+                quota: 5,
         },
         {
             id: 6,
@@ -67,6 +72,7 @@ export default function BookCollection({ search = "", category = "", years = "" 
             description:
                 "A magical story of Santiago, an Andalusian shepherd, and his quest to find a treasure that teaches him about life and destiny.",
             image: "https://images-na.ssl-images-amazon.com/images/I/51Z0nLAfLmL._SX331_BO1,204,203,200_.jpg",
+            quota: 0,
         },
         {
             id: 7,
@@ -77,6 +83,7 @@ export default function BookCollection({ search = "", category = "", years = "" 
             description:
                 "Explores how to cultivate focus in a distracted world and achieve exceptional work by avoiding shallow activities.",
             image: "https://nuriakenya.com/wp-content/uploads/2020/11/Deep-Work.jpg",
+            quota: 5,
         },
         {
             id: 8,
@@ -87,6 +94,7 @@ export default function BookCollection({ search = "", category = "", years = "" 
             description:
                 "A memoir of a woman who grows up in a strict and abusive household in rural Idaho but escapes through education.",
             image: "https://images-na.ssl-images-amazon.com/images/I/81WojUxbbFL.jpg",
+            quota: 5,
         },
         {
             id: 9,
@@ -97,6 +105,7 @@ export default function BookCollection({ search = "", category = "", years = "" 
             description:
                 "An ancient Chinese military treatise that provides strategies for warfare and how they can be applied to life and leadership.",
             image: "https://cdn2.penguin.com.au/covers/original/9780877734529.jpg",
+            quota: 0,
         },
         {
             id: 10,
@@ -107,6 +116,7 @@ export default function BookCollection({ search = "", category = "", years = "" 
             description:
                 "A classic American novel exploring themes of wealth, love, and the pursuit of the American Dream during the Jazz Age.",
             image: "https://i.pinimg.com/originals/a7/ea/79/a7ea79067b7149982f3856f6d85a22d0.jpg",
+            quota: 5,
         },
         {
             id: 11,
@@ -117,6 +127,7 @@ export default function BookCollection({ search = "", category = "", years = "" 
             description:
                 "A profound psychological memoir and exploration of the meaning of life, based on the author's experiences in a Nazi concentration camp.",
             image: "https://covers.shakespeareandcompany.com/97818460/9781846042843.jpg",
+            quota: 5,
         },
         {
             id: 12,
@@ -127,6 +138,7 @@ export default function BookCollection({ search = "", category = "", years = "" 
             description:
                 "The memoir of former First Lady Michelle Obama, chronicling her life from childhood to the White House.",
             image: "https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL.jpg",
+            quota: 5,
         },
     ];
 
@@ -158,71 +170,62 @@ export default function BookCollection({ search = "", category = "", years = "" 
                 </h2>
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-                {filteredBooks.length > 0 ? (
-                    filteredBooks.map((book) => (
-                        <div
-                            key={book.id}
-                            className="bg-white shadow-md rounded-lg overflow-hidden max-w-sm hover:shadow-lg hover:-translate-y-1 transition duration-300"
-                        >
-                            {/* Book Image */}
-                            <div className="relative group">
-                                <img
-                                    src={book.image}
-                                    alt={`${book.title} Cover`}
-                                    className="w-full h-48 md:h-96 object-cover group-hover:opacity-90 transition duration-300"
-                                />
-                                {/* years Badge */}
-                                <Link href={`/collections?year=${book.year}`}>
-                                    <span className="absolute bottom-10 left-2 bg-red-100 text-red-600 text-xs font-medium px-2 py-1 rounded-lg">
-                                        {book.year}
-                                    </span>
-                                </Link>
-                                {/* Category Badge */}
-                                <Link href={`/collections?category=${book.category}`}>
-                                    <span className="absolute bottom-2 left-2 bg-blue-100 text-blue-600 text-xs font-medium px-2 py-1 rounded-lg">
-                                        {book.category}
-                                    </span>
-                                </Link>
-                                {/* Bookmark Button */}
-                                <button className="absolute top-2 right-2 bg-white p-2 rounded-full shadow hover:text-red-500 transition duration-300">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1.5}
-                                        stroke="currentColor"
-                                        className="w-6 h-6 text-gray-600"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M5 3v18l7-5 7 5V3z"
-                                        />
-                                    </svg>
-                                </button>
-                            </div>
-
-                            {/* Card Content */}
-                            <div className="p-4">
-                                <Link href={`/collections/${book.id}`}>
-                                    {/* Title */}
-                                    <h2 className="text-lg font-bold text-gray-900 truncate hover:text-green-500 transition duration-300">
-                                        {book.title}
-                                    </h2>
-                                    {/* Author */}
-                                    <p className="text-sm text-gray-600 mt-1">by {book.author}</p>
-                                    {/* Description */}
-                                    <p className="text-sm text-gray-600 mt-3">{book.description}</p>
-                                </Link>
-                            </div>
-                        </div>
-                    ))
-                ) : (
-                    <p className="text-gray-500 text-center col-span-full">
-                        No books match your criteria.
-                    </p>
-                )}
+  {filteredBooks.length > 0 ? (
+    filteredBooks.map((book) => (
+      <div
+        key={book.id}
+        className="relative bg-white shadow-md rounded-lg overflow-hidden max-w-sm hover:shadow-lg hover:-translate-y-1 transition duration-300"
+      >
+        {/* Book Image */}
+        <div className="relative group">
+          <img
+            src={book.image}
+            alt={`${book.title} Cover`}
+            className="w-full h-48 md:h-96 object-cover group-hover:opacity-90 transition duration-300"
+          />
+          {/* Years Badge */}
+          <Link href={`/collections?year=${book.year}`}>
+            <span className="absolute bottom-10 left-2 bg-red-100 text-red-600 text-xs font-medium px-2 py-1 rounded-lg">
+              {book.year}
+            </span>
+          </Link>
+          {/* Category Badge */}
+          <Link href={`/collections?category=${book.category}`}>
+            <span className="absolute bottom-2 left-2 bg-blue-100 text-blue-600 text-xs font-medium px-2 py-1 rounded-lg">
+              {book.category}
+            </span>
+          </Link>
+          {/* Overlay for Out of Quota */}
+          {book.quota === 0 && (
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center backdrop-blur-sm">
+              <p className="text-white text-lg font-bold">Maaf (｡•́︿•̀｡) <br/> bukunya lagi banyak <br/> yang baca</p>
             </div>
+          )}
+        </div>
+
+        {/* Card Content */}
+        <div className="p-4">
+          <Link href={`/collections/${book.id}`}>
+            {/* Title */}
+            <h2 className="text-lg font-bold text-gray-900 truncate hover:text-green-500 transition duration-300">
+              {book.title}
+            </h2>
+            {/* Author */}
+            <p className="text-sm text-gray-600 mt-1">by {book.author}</p>
+            {/* Description */}
+            <p className="text-sm text-gray-600 mt-3">{book.description}</p>
+          </Link>
+        </div>
+      </div>
+    ))
+  ) : (
+    <p className="text-gray-500 text-center col-span-full">
+      No books match your criteria.
+    </p>
+  )}
+</div>
+
+
         </>
     );
 }
