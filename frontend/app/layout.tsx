04ae706061850e5c19b2f0c14172b67f57be8a74
Gagas Surya <gagassurya19@gmail.com>
2024-12-04 12:37:25 +0700

    change position toaster

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster richColors position="top-center" />
+        <Toaster richColors position="top-center" className="top-24"/>
       </body>
     </html>
   );
