d87a6873bc049149b2a3320ca004392c82a5ae0c
Akbar <149041645+rafiraihan27@users.noreply.github.com>
2024-12-18 16:07:08 +0700

    add func getStatisticBook (#6)
    Co-authored-by: Gagassurya19 <gagassurya19@gmail.com>

--- backend/src/main/java/com/tubesbookwise/Service/StatisticService.java
+++ backend/src/main/java/com/tubesbookwise/Service/StatisticService.java
@@ -4,19 +4,22 @@ import com.tubesbookwise.Repository.UserRepository;
 import com.tubesbookwise.Repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+    
 @Service
 public class StatisticService {
 
-        @Autowired
-        public UserRepository userRepository;
-        public Long getJumlahUserNow() {
-            return userRepository.count();
-        }
-        @Autowired
-        private BookRepository bookRepository;
+  @Autowired
+  public UserRepository userRepository;
+  
+  public Long getJumlahUserNow() {
+    return userRepository.count();
+  }
+  
+  @Autowired
+  private BookRepository bookRepository;
 
-        public long  getStatisticBook(){
-            return bookRepository.count();
-        }
+  public long  getStatisticBook(){
+    return bookRepository.count();
+  }
+  
 }
