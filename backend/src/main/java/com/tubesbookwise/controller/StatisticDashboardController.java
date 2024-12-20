31bc1fc92e53246457ebef6a48d2b34b07641b5b
Gagas Surya <gagassurya19@gmail.com>
2024-12-21 02:56:52 +0700

    integration-api: statistik

--- backend/src/main/java/com/tubesbookwise/controller/StatisticDashboardController.java
+++ backend/src/main/java/com/tubesbookwise/controller/StatisticDashboardController.java
@@ -2,10 +2,9 @@ package com.tubesbookwise.controller;
 
 import com.tubesbookwise.Service.StatisticService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Map;
@@ -14,20 +13,25 @@ import java.util.Map;
 @RequestMapping("/api/statistic")
 public class StatisticDashboardController {
     @Autowired
-        private StatisticService statisticservice;
+    private StatisticService statisticservice;
+
     //    GET http://localhost:8080/api/statistic
     @Operation(summary = "Ambil data statistik", description = "Mengambil data statistik dari database")
     @GetMapping()
-    public ResponseEntity<?> getStatistics() {
+    public ResponseEntity<?> getStatistics(
+            @RequestParam(value = "max", required = false) Integer max
+    ) {
         return ResponseEntity.ok().body(
                 Map.of(
                         "message", "Data statistik berhasil diambil",
                         "data", Map.of(
                                 "totalBook", statisticservice.getStatisticBook(),
                                 "totalUser", statisticservice.getJumlahUserNow(),
-                                "totalTransaction", 20,
-                                "averageReview", 3.9,
-                                "totalReview", statisticservice.getJumlahReview()
+                                "totalTransaction", statisticservice.getJumlahTransaksi(),
+                                "totalNotifications", statisticservice.getJumlahNotifikasi(),
+                                "averageReview", statisticservice.getAverageReview(),
+                                "totalReview", statisticservice.getJumlahReview(),
+                                "recentReviews", statisticservice.getRecentReview(max)
                         )
                 )
         );
