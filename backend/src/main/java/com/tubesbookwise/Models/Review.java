f5fc7d8e898b85c2abe21820ed7451e2014bdf19
magista <mcmagista02@gmail.com>
2024-12-23 12:03:07 +0700

    Add models review

--- backend/src/main/java/com/tubesbookwise/Models/Review.java
+++ backend/src/main/java/com/tubesbookwise/Models/Review.java
@@ -13,73 +13,73 @@ import java.util.UUID;
 @Table(name = "reviews")
 public class Review {
 
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.AUTO)
-    // @Column(name = "id", updatable = false, nullable = false)
-    // private UUID id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
 
-    // @ManyToOne(fetch = FetchType.EAGER)
-    // @JoinColumn(name = "book_id", nullable = false)
-    // private Book book;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
 
-    // @ManyToOne(fetch = FetchType.EAGER)
-    // @JoinColumn(name = "author_id", nullable = false)
-    // private User author;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
 
-    // @Column(name = "date", nullable = false)
-    // private LocalDateTime date;
+    @Column(name = "date", nullable = false)
+    private LocalDateTime date;
 
-    // @Column(name = "rating", nullable = false)
-    // private double rating;
+    @Column(name = "rating", nullable = false)
+    private double rating;
 
-    // @Column(name = "content", columnDefinition = "TEXT", nullable = false)
-    // private String content;
+    @Column(name = "content", columnDefinition = "TEXT", nullable = false)
+    private String content;
 
-    // public UUID getId() {
-    //     return id;
-    // }
+    public UUID getId() {
+        return id;
+    }
 
-    // public void setId(UUID id) {
-    //     this.id = id;
-    // }
+    public void setId(UUID id) {
+        this.id = id;
+    }
 
-    // public Book getBook() {
-    //     return book;
-    // }
+    public Book getBook() {
+        return book;
+    }
 
-    // public void setBook(Book book) {
-    //     this.book = book;
-    // }
+    public void setBook(Book book) {
+        this.book = book;
+    }
 
-    // public User getAuthor() {
-    //     return author;
-    // }
+    public User getAuthor() {
+        return author;
+    }
 
-    // public void setAuthor(User author) {
-    //     this.author = author;
-    // }
+    public void setAuthor(User author) {
+        this.author = author;
+    }
 
-    // public LocalDateTime getDate() {
-    //     return date;
-    // }
+    public LocalDateTime getDate() {
+        return date;
+    }
 
-    // public void setDate(LocalDateTime date) {
-    //     this.date = date;
-    // }
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
 
-    // public double getRating() {
-    //     return rating;
-    // }
+    public double getRating() {
+        return rating;
+    }
 
-    // public void setRating(double rating) {
-    //     this.rating = rating;
-    // }
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 
-    // public String getContent() {
-    //     return content;
-    // }
+    public String getContent() {
+        return content;
+    }
 
-    // public void setContent(String content) {
-    //     this.content = content;
-    // }
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
