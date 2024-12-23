85aaacab89d99fa87fb422fb035e6621e83a910e
unkownhunter <yustinus134@gmail.com>
2024-12-23 11:23:30 +0700

    fixed notifications model

--- backend/src/main/java/com/tubesbookwise/Models/Notification.java
+++ backend/src/main/java/com/tubesbookwise/Models/Notification.java
@@ -14,87 +14,87 @@ import java.util.UUID;
 @Table(name = "notifications")
 public class Notification {
 
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.UUID)
-    // private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
 
-    // @ManyToOne(fetch = FetchType.EAGER)
-    // @JoinColumn(name = "user_id", nullable = false)
-    // private User user;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
-    // @Column(name = "title", nullable = false)
-    // private String title;
+    @Column(name = "title", nullable = false)
+    private String title;
 
-    // @Column(name = "message", columnDefinition = "TEXT", nullable = false)
-    // private String message;
+    @Column(name = "message", columnDefinition = "TEXT", nullable = false)
+    private String message;
 
-    // @Enumerated(EnumType.STRING)
-    // @Column(name = "type", nullable = false)
-    // private NotificationType type;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private NotificationType type;
 
-    // @Column(name = "date", nullable = false)
-    // private LocalDateTime date;
+    @Column(name = "date", nullable = false)
+    private LocalDateTime date;
 
-    // @Column(name = "`read`", nullable = false)
-    // private boolean read = false;
+    @Column(name = "`read`", nullable = false)
+    private boolean read = false;
 
-    // public enum NotificationType {
-    //     INFO, REMINDER, ALERT
-    // }
+    public enum NotificationType {
+        INFO, REMINDER, ALERT
+    }
 
-    // public String getId() {
-    //     return id;
-    // }
+    public String getId() {
+        return id;
+    }
 
-    // public void setId(String id) {
-    //     this.id = id;
-    // }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    // public User getUser() {
-    //     return user;
-    // }
+    public User getUser() {
+        return user;
+    }
 
-    // public void setUser(User user) {
-    //     this.user = user;
-    // }
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-    // public String getTitle() {
-    //     return title;
-    // }
+    public String getTitle() {
+        return title;
+    }
 
-    // public void setTitle(String title) {
-    //     this.title = title;
-    // }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-    // public String getMessage() {
-    //     return message;
-    // }
+    public String getMessage() {
+        return message;
+    }
 
-    // public void setMessage(String message) {
-    //     this.message = message;
-    // }
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-    // public NotificationType getType() {
-    //     return type;
-    // }
+    public NotificationType getType() {
+        return type;
+    }
 
-    // public void setType(NotificationType type) {
-    //     this.type = type;
-    // }
+    public void setType(NotificationType type) {
+        this.type = type;
+    }
 
-    // public LocalDateTime getDate() {
-    //     return date;
-    // }
+    public LocalDateTime getDate() {
+        return date;
+    }
 
-    // public void setDate(LocalDateTime date) {
-    //     this.date = date;
-    // }
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
 
-    // public boolean isRead() {
-    //     return read;
-    // }
+    public boolean isRead() {
+        return read;
+    }
 
-    // public void setRead(boolean read) {
-    //     this.read = read;
-    // }
+    public void setRead(boolean read) {
+        this.read = read;
+    }
 }
